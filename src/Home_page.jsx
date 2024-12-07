392a6dced4c8ff9b572be151b03a827f9afcf164
Adi <adithya25032004@gmail.com>
2024-12-07 21:55:14 +0530

    changes

--- src/Home_page.jsx
+++ src/Home_page.jsx
@@ -1,6 +1,16 @@
 const Home = () => {
     return (
+        <>
         <section class="sec" id="home"> Adithya </section> 
+            <div class="home_container">
+                <p class="home_text">
+                    HII,My name is Adithya 
+                </p>
+                <p class="home_text">
+                    I am a Full Stack Developer
+                </p>    
+            </div>
+        </>
     )
 }
 
